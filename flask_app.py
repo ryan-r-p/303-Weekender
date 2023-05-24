113fc92945066a70de66af0e521d9bb38bcb5d09
Ryan Peet <118061227+ryan-r-p@users.noreply.github.com>
2023-05-24 17:32:10 -0600

    Converting pandas table to sqlite, ingraining in Flask logic

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,4 @@
+import numpy as np
 from flask import Flask, request
 import pandas as pd
 from twilio.twiml.messaging_response import MessagingResponse
@@ -17,17 +18,18 @@ def list_validity(ipt, valid_list):
     return lst
 
 
-def style_list(lst):
-    length = len(lst)
-    if length == 1:
-        return lst[0]
-    elif length == 2:
-        return f"{lst[0]} & {lst[1]}"
+def list_format(my_list):
+    if len(my_list) == 1:
+        return str(my_list[0])
     else:
-        return ", ".join(lst[:-1]) + f", & {lst[-1]}"
+        formatted_list = ', '.join(str(element) for element in my_list[:-1])
+        return f"{formatted_list} & {my_list[-1]}"
 
 
 app = Flask(__name__)
+
+# Creating the user_db if it does not already exist
+user_db.create_new_user_db_tbl_if_not_existing()
 
 
 @app.route('/sms/', methods=['POST'])
@@ -40,36 +42,42 @@ def incoming_sms():
     resp = MessagingResponse()
 
     def response_and_check(bdy, phn_num, response_var):
+        # Query the entire USER_DB from the user_data.db as it currently stands, set everything that doesn't exist to nan
+        user_acct_tbl = user_db.query_all_user_data()
+        user_acct_tbl = user_acct_tbl.replace({None: np.nan})
+
         # "Begin" logic
         begin_str = "Begin"
         begin_no_case = begin_str.casefold()
         bdy_no_case = bdy.casefold()
         if bdy_no_case == begin_no_case:
-            if len(user_db.user_acct_tbl) == 1:
-                user_list = user_db.user_acct_tbl["ID"].squeeze()
+            if len(user_acct_tbl) == 1:
+                user_list = user_acct_tbl["ID"].squeeze()
                 if int(phn_num[2:]) != user_list:
                     new_phone_num = phn_num
                     new_id = int(phn_num[2:])
-                    new_df = pd.DataFrame({'ID': [new_id], 'PHONE_NUM': [new_phone_num], 'FIRST_NAME': [""], 'HAS_DAY_LIST': [0], "DAY_LIST": [""], "HAS_COURSE_LIST": [0], 'COURSE_LIST': [""], 'ACTIVE': [0]})
-                    user_db.user_acct_tbl = pd.concat([user_db.user_acct_tbl, new_df])
+                    new_df = pd.DataFrame({'ID': [new_id], 'PHONE_NUM': [new_phone_num], 'FIRST_NAME': [np.nan], 'HAS_DAY_LIST': [0], "DAY_LIST_1": [np.nan], "DAY_LIST_2": [np.nan], "DAY_LIST_3": [np.nan], "DAY_LIST_4": [np.nan], "HAS_COURSE_LIST": [0], 'COURSE_LIST_1': [np.nan], 'COURSE_LIST_2': [np.nan], 'COURSE_LIST_3': [np.nan], 'COURSE_LIST_4': [np.nan], 'COURSE_LIST_5': [np.nan], 'COURSE_LIST_6': [np.nan], 'COURSE_LIST_7': [np.nan], 'COURSE_LIST_8': [np.nan], 'COURSE_LIST_9': [np.nan], 'COURSE_LIST_10': [np.nan], 'ACTIVE': [0]})
+                    user_acct_tbl = pd.concat([user_acct_tbl, new_df])
+                    user_db.insert_id_and_phone_num(new_id, new_phone_num)
                     response_var.message(f"Welcome to 303-Weekender. Please respond to the following prompts to choose your settings:")
                 elif int(phn_num[2:]) == user_list:
                     response_var.message(f"You are already subscribed to 303-Weekender. Respond with STOP to unsubscribe")
             else:
-                user_series = user_db.user_acct_tbl["ID"].squeeze()
+                user_series = user_acct_tbl["ID"].squeeze()
                 user_list = user_series.tolist()
                 if int(phn_num[2:]) not in user_list:
                     new_phone_num = phn_num
                     new_id = int(phn_num[2:])
-                    new_df = pd.DataFrame({'ID': [new_id], 'PHONE_NUM': [new_phone_num], 'FIRST_NAME': [""], 'HAS_DAY_LIST': [0], "DAY_LIST": [""], "HAS_COURSE_LIST": [0], 'COURSE_LIST': [""], 'ACTIVE': [0]})
-                    user_db.user_acct_tbl = pd.concat([user_db.user_acct_tbl, new_df])
+                    new_df = pd.DataFrame({'ID': [new_id], 'PHONE_NUM': [new_phone_num], 'FIRST_NAME': [np.nan], 'HAS_DAY_LIST': [0], "DAY_LIST_1": [np.nan], "DAY_LIST_2": [np.nan], "DAY_LIST_3": [np.nan], "DAY_LIST_4": [np.nan], "HAS_COURSE_LIST": [0], 'COURSE_LIST_1': [np.nan], 'COURSE_LIST_2': [np.nan], 'COURSE_LIST_3': [np.nan], 'COURSE_LIST_4': [np.nan], 'COURSE_LIST_5': [np.nan], 'COURSE_LIST_6': [np.nan], 'COURSE_LIST_7': [np.nan], 'COURSE_LIST_8': [np.nan], 'COURSE_LIST_9': [np.nan], 'COURSE_LIST_10': [np.nan], 'ACTIVE': [0]})
+                    user_acct_tbl = pd.concat([user_acct_tbl, new_df])
+                    user_db.insert_id_and_phone_num(new_id, new_phone_num)
                     response_var.message(f"Welcome to 303-Weekender. Please respond to the following prompts to choose your settings:")
                 elif int(phn_num[2:]) in user_list:
-                    response_var.message(f"You are already subscribed to 303-Weekender. Respond with STOP to unsubscribe")
+                    response_var.message(f"You are already subscribed to 303-Weekender. Respond with 'unsub' to unsubscribe")
 
         # Creating phone number index from user account table, using it for lookups
-        user_acct_tbl_index = user_db.user_acct_tbl.set_index('ID')
         new_id = int(phn_num[2:])
+        user_acct_tbl_index = user_acct_tbl.set_index('ID')
 
         if user_acct_tbl_index.index.isin([new_id]).any():
             # Config logic
@@ -77,38 +85,56 @@ def incoming_sms():
             config_str = config_str.casefold()
             if bdy_no_case == config_str:
                 user_acct_tbl_index.at[new_id, 'HAS_DAY_LIST'] = 0
-                user_acct_tbl_index.at[new_id, 'DAY_LIST'] = ""
+                user_acct_tbl_index.at[new_id, 'DAY_LIST_1'] = np.nan
+                user_acct_tbl_index.at[new_id, 'DAY_LIST_2'] = np.nan
+                user_acct_tbl_index.at[new_id, 'DAY_LIST_3'] = np.nan
+                user_acct_tbl_index.at[new_id, 'DAY_LIST_4'] = np.nan
                 user_acct_tbl_index.at[new_id, 'HAS_COURSE_LIST'] = 0
-                user_acct_tbl_index.at[new_id, 'COURSE_LIST'] = ""
+                user_acct_tbl_index.at[new_id, 'COURSE_LIST_1'] = np.nan
+                user_acct_tbl_index.at[new_id, 'COURSE_LIST_2'] = np.nan
+                user_acct_tbl_index.at[new_id, 'COURSE_LIST_3'] = np.nan
+                user_acct_tbl_index.at[new_id, 'COURSE_LIST_4'] = np.nan
+                user_acct_tbl_index.at[new_id, 'COURSE_LIST_5'] = np.nan
+                user_acct_tbl_index.at[new_id, 'COURSE_LIST_6'] = np.nan
+                user_acct_tbl_index.at[new_id, 'COURSE_LIST_7'] = np.nan
+                user_acct_tbl_index.at[new_id, 'COURSE_LIST_8'] = np.nan
+                user_acct_tbl_index.at[new_id, 'COURSE_LIST_9'] = np.nan
+                user_acct_tbl_index.at[new_id, 'COURSE_LIST_10'] = np.nan
                 user_acct_tbl_index.at[new_id, 'ACTIVE'] = 0
-                user_db.update_user_acct_table(user_acct_tbl_index)
+                user_db.enter_config(new_id)
 
             # Getting the responses from the text, converting those responses for use in the program and rejecting responses that don't fit, storing the user information in the user account table
             if user_acct_tbl_index.at[new_id, 'HAS_COURSE_LIST'] == 0 and user_acct_tbl_index.at[new_id, 'HAS_DAY_LIST'] == 1 and bdy_no_case != begin_no_case and bdy_no_case != config_str:
                 valid_course_list = ["Aurora Golf Courses", "Broken Tee", "CommonGround", "Denver Golf Courses", "DU Golf Club", "Foothills Recreation District", "Fossil Trace", "Green Valley Ranch", "Hyland Hills", "Indian Tree", "Lakewood Golf Courses", "Raccoon Creek", "Riverdale", "Saddleback", "Thorncreek", "West Woods"]
                 course_list_valid = list_validity(str(bdy), valid_course_list)
-                if course_list_valid != "Fail":
-                    user_acct_tbl_index.at[new_id, 'COURSE_LIST'] = course_list_valid
+                if course_list_valid != "Fail" and len(course_list_valid) <= 10:
+                    i = 1
+                    for course in course_list_valid:
+                        user_acct_tbl_index.at[new_id, f'COURSE_LIST_{i}'] = course
+                        i += 1
                     user_acct_tbl_index.at[new_id, 'HAS_COURSE_LIST'] = 1
-                    user_db.update_user_acct_table(user_acct_tbl_index)
+                    user_db.insert_course_list(course_list_valid, new_id)
                 else:
                     response_var.message(f"Invalid response. One or more courses from the course list were entered incorrectly")
-            if user_acct_tbl_index.at[new_id, 'HAS_DAY_LIST'] == 0 and user_acct_tbl_index.at[new_id, 'FIRST_NAME'] != "" and bdy_no_case != begin_no_case and bdy_no_case != config_str:
+            if user_acct_tbl_index.at[new_id, 'HAS_DAY_LIST'] == 0 and pd.notna(user_acct_tbl_index['FIRST_NAME'][new_id]) and bdy_no_case != begin_no_case and bdy_no_case != config_str:
                 valid_day_list = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
                 day_list_valid = list_validity(str(bdy), valid_day_list)
                 if day_list_valid != "Fail" and len(day_list_valid) <= 4:
-                    user_acct_tbl_index.at[new_id, 'DAY_LIST'] = day_list_valid
+                    i = 1
+                    for day in day_list_valid:
+                        user_acct_tbl_index.at[new_id, f'DAY_LIST_{i}'] = day
+                        i += 1
                     user_acct_tbl_index.at[new_id, 'HAS_DAY_LIST'] = 1
-                    user_db.update_user_acct_table(user_acct_tbl_index)
+                    user_db.insert_day_list(day_list_valid, new_id)
                 else:
                     response_var.message(f"Invalid response. Please ensure you are entering the day of the week with proper capitalization. Up to four selections are allowed.")
-            if user_acct_tbl_index.at[new_id, 'FIRST_NAME'] == "" and bdy_no_case != begin_no_case and bdy_no_case != config_str:
+            if pd.isna(user_acct_tbl_index['FIRST_NAME'][new_id]) and bdy_no_case != begin_no_case and bdy_no_case != config_str:
                 user_acct_tbl_index.at[new_id, 'FIRST_NAME'] = str(bdy)
-                user_db.update_user_acct_table(user_acct_tbl_index)
+                user_db.insert_first_name(str(bdy), new_id)
 
             # Sending the prompts to the user once tasks have been completed
             if user_acct_tbl_index.at[new_id, 'HAS_DAY_LIST'] == 1 and user_acct_tbl_index.at[new_id, 'HAS_COURSE_LIST'] == 0:
-                response_var.message(f"From the following list, enter the courses that you would like to receive SMS notifications for, separated by commas, when tee times go live:")
+                response_var.message(f"From the following list, enter the courses that you would like to receive SMS notifications for (separated by commas) when tee times go live. Enter up to 10 courses.")
                 response_var.message(f"(Please ensure exact spelling as seen here within the following prompts)")
                 response_var.message(f"Aurora Golf Courses\n"
                                      f"Broken Tee\n"
@@ -126,35 +152,37 @@ def incoming_sms():
                                      f"Saddleback\n"
                                      f"Thorncreek\n"
                                      f"West Woods")
-            if user_acct_tbl_index.at[new_id, 'FIRST_NAME'] != "" and user_acct_tbl_index.at[new_id, 'HAS_DAY_LIST'] == 0:
+            if pd.notna(user_acct_tbl_index['FIRST_NAME'][new_id]) and user_acct_tbl_index.at[new_id, 'HAS_DAY_LIST'] == 0:
                 response_var.message(f"Hi {user_acct_tbl_index.at[new_id, 'FIRST_NAME']}, provide the days of the week you want tee times on, separated by commas. You can select up to four days.\n"
                                      f"\n"
                                      f'(ex. "Friday, Sunday")')
-            if user_acct_tbl_index.at[new_id, 'FIRST_NAME'] == "":
+            if pd.isna(user_acct_tbl_index['FIRST_NAME'][new_id]):
                 response_var.message(f"What is your first name?")
 
             # Sending subscription notification
-            if user_acct_tbl_index.at[new_id, 'FIRST_NAME'] != "" and user_acct_tbl_index.at[new_id, 'HAS_DAY_LIST'] == 1 and user_acct_tbl_index.at[new_id, 'HAS_COURSE_LIST'] == 1:
+            if pd.notna(user_acct_tbl_index['FIRST_NAME'][new_id]) and user_acct_tbl_index.at[new_id, 'HAS_DAY_LIST'] == 1 and user_acct_tbl_index.at[new_id, 'HAS_COURSE_LIST'] == 1:
                 if user_acct_tbl_index.at[new_id, 'ACTIVE'] == 0:
-                    response_var.message(f'Congrats. You are now set to receive notifications for {style_list(user_acct_tbl_index.at[new_id, "DAY_LIST"])} tee times at the following courses: {style_list(user_acct_tbl_index.at[new_id, "COURSE_LIST"])}.\n'
+                    df2 = user_db.get_user_day_lists(new_id)
+                    day_list = df2['DAY_LIST'].tolist()
+                    df3 = user_db.get_user_course_lists(new_id)
+                    course_list = df3['COURSE_LIST'].tolist()
+                    response_var.message(f'Congrats. You are now set to receive notifications for {list_format(day_list)} tee times at the following courses: {list_format(course_list)}.\n'
                                          f'\n'
                                          f"Respond 'config' to change your notification settings\n"
                                          f"Respond 'unsub' to unsubscribe")
                 user_acct_tbl_index.at[new_id, 'ACTIVE'] = 1
-                user_db.update_user_acct_table(user_acct_tbl_index)
+                user_db.set_active(new_id)
 
             # Unsub logic
             if user_acct_tbl_index.at[new_id, 'ACTIVE'] == 1:
                 unsub_str = "Unsub"
                 unsub_str = unsub_str.casefold()
                 if bdy_no_case == unsub_str:
-                    user_db.user_unsub(new_id)
+                    user_db.unsub(new_id)
                     response_var.message('You are now unsubscribed. Respond with "begin" to re-subscribe.')
         else:
             response_var.message('Invalid response. Please subscribe first.')
-
     response_and_check(body, phone_num, resp)
-    print(user_db.user_acct_tbl.to_string())
     return str(resp)
 
 
--- user_db.py
+++ user_db.py
@@ -1,18 +1,147 @@
+import sqlite3
 import pandas as pd
 
-user_acct_tbl = pd.DataFrame(columns=["ID", "PHONE_NUM", "FIRST_NAME", "HAS_DAY_LIST", "DAY_LIST", "HAS_COURSE_LIST", "COURSE_LIST", "ACTIVE"])
+cxn = sqlite3.connect('user_data.db', check_same_thread=False)
+curs = cxn.cursor()
 
 
-def update_user_acct_table(user_account_tbl_w_indexes):
-    global user_acct_tbl
-    user_account_tbl_w_indexes = user_account_tbl_w_indexes.reset_index()
-    user_acct_tbl = user_account_tbl_w_indexes
+def create_new_user_db_tbl_if_not_existing():
+    query = f"""CREATE TABLE IF NOT EXISTS USER_DB (
+                    ID INTEGER PRIMARY KEY,
+                    PHONE_NUM TEXT,
+                    FIRST_NAME TEXT,
+                    HAS_DAY_LIST INTEGER,
+                    DAY_LIST_1 TEXT,
+                    DAY_LIST_2 TEXT,
+                    DAY_LIST_3 TEXT,
+                    DAY_LIST_4 TEXT,
+                    HAS_COURSE_LIST INTEGER,
+                    COURSE_LIST_1 TEXT,
+                    COURSE_LIST_2 TEXT,
+                    COURSE_LIST_3 TEXT,
+                    COURSE_LIST_4 TEXT,
+                    COURSE_LIST_5 TEXT,
+                    COURSE_LIST_6 TEXT,
+                    COURSE_LIST_7 TEXT,
+                    COURSE_LIST_8 TEXT,
+                    COURSE_LIST_9 TEXT,
+                    COURSE_LIST_10 TEXT,
+                    ACTIVE INTEGER  
+                    )"""
+    curs.execute(query)
+    cxn.commit()
 
 
-def user_unsub(phn_id):
-    global user_acct_tbl
-    i = user_acct_tbl.set_index('ID')
-    i = i.drop(phn_id)
-    i.reset_index(inplace=True)
-    user_acct_tbl = i
+def query_all_user_data():
+    query = "SELECT * FROM USER_DB"
+    df = pd.read_sql_query(query, cxn)
+    return df
 
+
+def get_user_course_lists(new_id):
+    query = f""" SELECT ID, COURSE_LIST_1 AS COURSE_LIST FROM USER_DB WHERE ID = {new_id} AND COURSE_LIST_1 IS NOT NULL
+    UNION ALL
+    SELECT ID, COURSE_LIST_2 AS COURSE_LIST FROM USER_DB WHERE ID = {new_id} AND COURSE_LIST_2 IS NOT NULL
+    UNION ALL
+    SELECT ID, COURSE_LIST_3 AS COURSE_LIST FROM USER_DB WHERE ID = {new_id} AND COURSE_LIST_3 IS NOT NULL
+    UNION ALL
+    SELECT ID, COURSE_LIST_4 AS COURSE_LIST FROM USER_DB WHERE ID = {new_id} AND COURSE_LIST_4 IS NOT NULL
+    UNION ALL
+    SELECT ID, COURSE_LIST_5 AS COURSE_LIST FROM USER_DB WHERE ID = {new_id} AND COURSE_LIST_5 IS NOT NULL
+    UNION ALL
+    SELECT ID, COURSE_LIST_6 AS COURSE_LIST FROM USER_DB WHERE ID = {new_id} AND COURSE_LIST_6 IS NOT NULL
+    UNION ALL
+    SELECT ID, COURSE_LIST_7 AS COURSE_LIST FROM USER_DB WHERE ID = {new_id} AND COURSE_LIST_7 IS NOT NULL
+    UNION ALL
+    SELECT ID, COURSE_LIST_8 AS COURSE_LIST FROM USER_DB WHERE ID = {new_id} AND COURSE_LIST_8 IS NOT NULL
+    UNION ALL
+    SELECT ID, COURSE_LIST_9 AS COURSE_LIST FROM USER_DB WHERE ID = {new_id} AND COURSE_LIST_9 IS NOT NULL
+    UNION ALL
+    SELECT ID, COURSE_LIST_10 AS COURSE_LIST FROM USER_DB WHERE ID = {new_id} AND COURSE_LIST_10 IS NOT NULL
+    """
+    df = pd.read_sql_query(query, cxn)
+    return df
+
+
+def get_user_day_lists(new_id):
+    query = f""" SELECT ID, DAY_LIST_1 AS DAY_LIST FROM USER_DB WHERE ID = {new_id} AND DAY_LIST_1 IS NOT NULL
+    UNION ALL
+    SELECT ID, DAY_LIST_2 AS DAY_LIST FROM USER_DB WHERE ID = {new_id} AND DAY_LIST_2 IS NOT NULL
+    UNION ALL
+    SELECT ID, DAY_LIST_3 AS DAY_LIST FROM USER_DB WHERE ID = {new_id} AND DAY_LIST_3 IS NOT NULL
+    UNION ALL
+    SELECT ID, DAY_LIST_4 AS DAY_LIST FROM USER_DB WHERE ID = {new_id} AND DAY_LIST_4 IS NOT NULL
+    """
+    df = pd.read_sql_query(query, cxn)
+    return df
+
+
+def insert_id_and_phone_num(new_id, new_phone):
+    query = """INSERT INTO USER_DB (ID, PHONE_NUM, HAS_DAY_LIST, HAS_COURSE_LIST, ACTIVE) VALUES (?, ?, 0, 0, 0)"""
+    curs.execute(query, (new_id, new_phone))
+    cxn.commit()
+
+
+def insert_first_name(first_name, new_id):
+    query = """UPDATE USER_DB SET FIRST_NAME = ? WHERE ID = ?"""
+    curs.execute(query, (first_name, new_id))
+    cxn.commit()
+
+
+def insert_day_list(user_day_list, new_id):
+    i = 1
+    query1 = f"""UPDATE USER_DB SET HAS_DAY_LIST = 1 WHERE ID = ?"""
+    curs.execute(query1, (new_id,))
+    cxn.commit()
+    for day in user_day_list:
+        query2 = f"""UPDATE USER_DB SET DAY_LIST_{i} = ? WHERE ID = ?"""
+        curs.execute(query2, (day, new_id))
+        cxn.commit()
+        i += 1
+
+
+def insert_course_list(user_course_list, new_id):
+    i = 1
+    query1 = f"""UPDATE USER_DB SET HAS_COURSE_LIST = 1 WHERE ID = ?"""
+    curs.execute(query1, (new_id,))
+    cxn.commit()
+    for course in user_course_list:
+        query = f"""UPDATE USER_DB SET COURSE_LIST_{i} = ? WHERE ID = ?"""
+        curs.execute(query, (course, new_id))
+        cxn.commit()
+        i += 1
+
+
+def set_active(new_id):
+    query = f"""UPDATE USER_DB SET ACTIVE = 1 WHERE ID = {new_id}"""
+    curs.execute(query)
+    cxn.commit()
+
+
+def enter_config(new_id):
+    query = """UPDATE USER_DB SET HAS_DAY_LIST = 0, 
+                                  DAY_LIST_1 = NULL,
+                                  DAY_LIST_2 = NULL,
+                                  DAY_LIST_3 = NULL,
+                                  DAY_LIST_4 = NULL,
+                                  HAS_COURSE_LIST = 0,
+                                  COURSE_LIST_1 = NULL,
+                                  COURSE_LIST_2 = NULL,
+                                  COURSE_LIST_3 = NULL,
+                                  COURSE_LIST_4 = NULL,
+                                  COURSE_LIST_5 = NULL,
+                                  COURSE_LIST_6 = NULL,
+                                  COURSE_LIST_7 = NULL,
+                                  COURSE_LIST_8 = NULL,
+                                  COURSE_LIST_9 = NULL,
+                                  COURSE_LIST_10 = NULL,
+                                  ACTIVE = 0
+                WHERE ID = ?"""
+    curs.execute(query, (new_id,))
+    cxn.commit()
+
+
+def unsub(new_id):
+    query = "DELETE FROM USER_DB WHERE ID = ?"
+    curs.execute(query, (new_id,))
+    cxn.commit()
